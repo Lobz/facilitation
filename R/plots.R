1197fdb62ac05fc42893e216a3a54ecf13f95e52
Andre Chalom <andrechalom@gmail.com>
2015-11-30 13:40:53 -0200

    stackplot(log=T) working as intended

--- R/plots.R
+++ R/plots.R
@@ -32,11 +32,11 @@ stackplot <- function(mat, col, legend, log.y = FALSE, ...) {
   time <- as.numeric(rownames(mat))
   for (i in (N-1):1) # sums populations IN REVERSE, may cause problems for 1 stages only
     mat[,i] = mat[,i] + mat[,i+1]
-  mat <- cbind(mat, rep(0, length(time)))
+  mat <- cbind(mat, rep(minp, length(time)))
   # maximo da escala do grafico
   maxp <-max(mat[,1])
 
-  plot(0, type='n', ylim=c(minp, maxp), xlim=c(0, max(time)), ylab="Population", xlab="Time", main="Facilitation dynamics", log=log, ...)
+  plot(1, type='n', ylim=c(minp, maxp), xlim=c(0, max(time)), ylab="Population", xlab="Time", main="Facilitation dynamics", log=log, ...)
   x <- c(time, rev(time))
   for (i in 1:(N)) {
     y <- c(mat[,i], rev(mat[,i+1]))
