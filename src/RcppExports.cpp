8100787f847d8bf2e9b2fbdf53c1d006c5eb0c21
Oz <marinacs@gmail.com>
2016-02-04 15:29:24 -0200

    remove test_basic

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,32 +5,20 @@
 
 using namespace Rcpp;
 
-// test_basic
-Rcpp::List test_basic(std::string filename, std::string outfilename);
-RcppExport SEXP facilitation_test_basic(SEXP filenameSEXP, SEXP outfilenameSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject __result;
-    Rcpp::RNGScope __rngScope;
-    Rcpp::traits::input_parameter< std::string >::type filename(filenameSEXP);
-    Rcpp::traits::input_parameter< std::string >::type outfilename(outfilenameSEXP);
-    __result = Rcpp::wrap(test_basic(filename, outfilename));
-    return __result;
-END_RCPP
-}
 // test_parameter
-Rcpp::List test_parameter(Rcpp::NumericVector times, int num_stages, Rcpp::NumericVector parameters, double f, Rcpp::IntegerVector init, double w, double h);
-RcppExport SEXP facilitation_test_parameter(SEXP timesSEXP, SEXP num_stagesSEXP, SEXP parametersSEXP, SEXP fSEXP, SEXP initSEXP, SEXP wSEXP, SEXP hSEXP) {
+Rcpp::List test_parameter(Rcpp::NumericVector times, int num_stages, Rcpp::NumericVector parameters, Rcpp::NumericVector fac, Rcpp::IntegerVector init, double w, double h);
+RcppExport SEXP facilitation_test_parameter(SEXP timesSEXP, SEXP num_stagesSEXP, SEXP parametersSEXP, SEXP facSEXP, SEXP initSEXP, SEXP wSEXP, SEXP hSEXP) {
 BEGIN_RCPP
     Rcpp::RObject __result;
     Rcpp::RNGScope __rngScope;
     Rcpp::traits::input_parameter< Rcpp::NumericVector >::type times(timesSEXP);
     Rcpp::traits::input_parameter< int >::type num_stages(num_stagesSEXP);
     Rcpp::traits::input_parameter< Rcpp::NumericVector >::type parameters(parametersSEXP);
-    Rcpp::traits::input_parameter< double >::type f(fSEXP);
+    Rcpp::traits::input_parameter< Rcpp::NumericVector >::type fac(facSEXP);
     Rcpp::traits::input_parameter< Rcpp::IntegerVector >::type init(initSEXP);
     Rcpp::traits::input_parameter< double >::type w(wSEXP);
     Rcpp::traits::input_parameter< double >::type h(hSEXP);
-    __result = Rcpp::wrap(test_parameter(times, num_stages, parameters, f, init, w, h));
+    __result = Rcpp::wrap(test_parameter(times, num_stages, parameters, fac, init, w, h));
     return __result;
 END_RCPP
 }
--- src/main.cpp
+++ src/main.cpp
@@ -40,51 +40,6 @@ status_list run_tests(bool print, int ntimes,double * times, int num_stages, dou
 }
 
 // [[Rcpp::export]]
-Rcpp::List test_basic(std::string filename,std::string outfilename = ""){
-
-	Rcpp::List ret;
-	int num_stages, h, w, i, *init,ntime=50;
-	double fac, *par, *time;
-	bool out = (outfilename!="");
-
-	std::ifstream inputfile(filename);
-	std::ofstream outputfile(out? outfilename : "output_default.txt");
-	auto coutbuf = std::cout.rdbuf(outputfile.rdbuf()); //save and redirect output
-
-	if(!inputfile) {
-		std::cerr << "#file \""<< filename << "\"not found\n";
-		return NULL;
-	}
-	
-	std::cout << "#how many stages?\n";
-	inputfile >> num_stages;
-	std::cout << "#supply width, height and facilitation parameter.\n";
-	inputfile >> w; inputfile >> h; inputfile >> fac;
-	std::cout << "#facilitation parameter: " << fac << "\n";
-	std::cout << "#supply N+1 parameter matrix in lines of 'G R D Radius'\n";
-	par = (double*)malloc((num_stages+1)*4*(sizeof(double*)));
-	for(i=0;i<num_stages+1;i++){
-		inputfile >> par[i*4+0]; inputfile >> par[i*4+1]; inputfile >> par[i*4+2]; inputfile >> par[i*4+3];
-	}
-	std::cout << "#supply initial populations\n";
-	init = (int*)malloc((num_stages+1)*(sizeof(int)));
-	for(i=0;i<num_stages+1;i++){
-		inputfile >>init[i];
-	}
-	inputfile.close();
-	std::cout << "#okay!\n";
-
-	time = (double*) malloc(ntime*sizeof(double));
-	for(i=1,time[0]=0;i<ntime;i++) time[i] = time[i-1] + 0.1;
-	ret = run_tests(out,ntime,time,num_stages,par,fac,w,h, init);
-
-	if(out) std::cout.rdbuf(coutbuf); //reset to standard output again
-
-	return ret;
-
-}
-
-// [[Rcpp::export]]
 Rcpp::List test_parameter(Rcpp::NumericVector times, int num_stages,Rcpp::NumericVector parameters, Rcpp::NumericVector fac, Rcpp::IntegerVector init, double w=10, double h=10){
 	int *in;
 	Rcpp::List ret;
