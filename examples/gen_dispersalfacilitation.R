d6d1cb85a545b4d769f9c852688ce56dfc25756f
Oz <marinacs@gmail.com>
2016-06-10 16:55:25 -0300

    corrected example?

--- examples/gen_dispersalfacilitation.R
+++ examples/gen_dispersalfacilitation.R
@@ -1,71 +1,49 @@
 library(animation)
+#1/(2+1)=1/3
+#.2/(.2+1.4)=1/8
+#s=1/24
+#Rs=.25
+#.2/(.2+.2)=1/2
+#s=1/6
+#Rs=1
 numstages <- 3
-deathrates <- c(2, 1.2, 0.6)  # death rates for seed, sapling and adult
+deathrates <- c(2, 1.4, 0.5)  # death rates for seed, sapling and adult
 growthrates <- c(1, 0.2)      # transition rates seed-->sapling and sapling-->adult
-reproductionrate <- 10        # reproduction rate (only adult)
+reproductionrate <- 6        # reproduction rate (only adult)
 dispersalradius <- 1.2          # average distance a seed falls from the parent (distance is gaussian)
-times <- seq(0,120,length.out=60)         # array of times of interest
 initialpop <- c(0,0,15,10)    # initial pop. sizes for the 3 stages plus the facilitator species
-facindex <- c(0,1)            # this will be the values by which facilitator decreases seeds and seedlings deathrates
-effects <- c(0,0,0, 0,-0.5,0, 0,0,-0.2) # the effects reducing deathrate (negative values increase deathrates)
+facindex <- c(0,1.2)            # this will be the values by which facilitator decreases seeds and seedlings deathrates
+effects <- c(0,0,0, 0,-0.5,0, 0,0,-1) # the effects reducing deathrate (negative values increase deathrates)
 radius <- c(0,0.2,2,3)        # this are the distances up to which the individuals can have effect on others, by stage + facilitator
 h <- 40                       # arena height
 w <- 40                       # arena width
 
-dispersalradius <- 0.2          # average distance a seed falls from the parent (distance is gaussian)
-dt1 <- facByRates(times=times, n=numstages, Ds=deathrates, Gs=growthrates, dispersal=dispersalradius, R=reproductionrate, 
-		 interactions=effects, fac=facindex, init=initialpop, rad=radius, h=h, w=w)
+maxt <- 150
+maxt=5
+wrapper <- function(disp){ set.seed(1234)
+	facByRates(maxt, n=numstages, Ds=deathrates, Gs=growthrates, dispersal=disp, R=reproductionrate, 
+		 interactions=effects, fac=facindex, init=initialpop, rad=radius, h=h, w=w)}
 
-ab1 <- abundance_matrix(dt1$data)
-stackplot(ab1[,1:3])
-savePlot("disperse1.png")
+library(parallel)
 
-dispersalradius <- 1.5          # average distance a seed falls from the parent (distance is gaussian)
-dt2 <- facByRates(times=times, n=numstages, Ds=deathrates, Gs=growthrates, dispersal=dispersalradius, R=reproductionrate, 
-		 interactions=effects, fac=facindex, init=initialpop, rad=radius, h=h, w=w)
+dispersions <- .2*2^(1:6)
+results <- mclapply(dispersions,wrapper)
 
-ab2 <- abundance_matrix(dt2$data)
-stackplot(ab2[,1:3])
-savePlot("disperse2.png")
+details=10
+times <- seq(0,maxt,length.out=details)         # array of times of interest
+abmatrices <- mclapply(results,function(r){abundance_matrix(r,times)[,1:3]})
 
-dispersalradius <- 3          # average distance a seed falls from the parent (distance is gaussian)
-dt3 <- facByRates(times=times, n=numstages, Ds=deathrates, Gs=growthrates, dispersal=dispersalradius, R=reproductionrate, 
-		 interactions=effects, fac=facindex, init=initialpop, rad=radius, h=h, w=w)
+poptots <- lapply(abmatrices,rowSums)
+# PLOT TOGHETER 
+colors <- colorRampPalette(c("red","blue4"))(length(dispersions))
 
-ab3 <- abundance_matrix(dt3$data)
-stackplot(ab3[,1:3])
-savePlot("disperse3.png")
+maxpop = max(lapply(poptots,max))
+plot(NULL,NULL,ylim=c(0,maxpop),xlim=c(0,maxt),ylab="População",xlab="Tempo",main="População total para raios de dispersão variados")
 
-dispersalradius <- 6          # average distance a seed falls from the parent (distance is gaussian)
-dt4 <- facByRates(times=times, n=numstages, Ds=deathrates, Gs=growthrates, dispersal=dispersalradius, R=reproductionrate, 
-		 interactions=effects, fac=facindex, init=initialpop, rad=radius, h=h, w=w)
+for(i in 1:length(dispersions)){
+	x <- poptots[[i]]
+	lines(x~times,col=colors[i],lwd=1.2)
+}
 
-ab4 <- abundance_matrix(dt4$data)
-stackplot(ab4[,1:3])
-savePlot("disperse4.png")
-
-dispersalradius <- 12          # average distance a seed falls from the parent (distance is gaussian)
-dt5 <- facByRates(times=times, n=numstages, Ds=deathrates, Gs=growthrates, dispersal=dispersalradius, R=reproductionrate, 
-		 interactions=effects, fac=facindex, init=initialpop, rad=radius, h=h, w=w)
-
-ab5 <- abundance_matrix(dt5$data)
-stackplot(ab5[,1:3])
-savePlot("disperse5.png")
-
-col <- colorRampPalette(c("red","blue4"))(5)
-y = c(0,max(c(rowSums(ab1),rowSums(ab2),rowSums(ab3),rowSums(ab4),rowSums(ab5))))
-plot(rowSums(ab5)~rownames(ab5),ylim=y,xlim=c(min(times),max(times)),type="l",lwd=1.2,ylab="População",xlab="Tempo",col=col[5],main="Efeito da dispersão")
-lines(rowSums(ab4)~rownames(ab4),col=col[4])
-lines(rowSums(ab3)~rownames(ab3),col=col[3])
-lines(rowSums(ab2)~rownames(ab2),col=col[2])
-lines(rowSums(ab1)~rownames(ab1),col=col[1])
-legend("topleft", legend=c("12","6","3","1.5","0.2"), fill=rev(col))
+legend("topleft", legend=dispersions, fill=colors)
 savePlot("dispersesumK.png")
-
-fitlm <- function(ab){coef(lm(rowSums(ab[,1:3])~as.numeric(rownames(ab))))[2]}
-rates <- c(0,0,0,0,0)
-rates[1] <- fitlm(ab1)
-rates[2] <- fitlm(ab2)
-rates[3] <- fitlm(ab3)
-rates[4] <- fitlm(ab4)
-rates[5] <- fitlm(ab5)
