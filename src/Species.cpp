d00b21aa9a3e85c2862533650753d8f42d955a5d
Oz <marinacs@gmail.com>
2017-03-18 23:50:08 -0300

    ooops. correcting leftover bug from previous merge

--- src/Species.cpp
+++ src/Species.cpp
@@ -5,141 +5,143 @@
 Species::Species(Arena *ar,int myid, double *par) : Species(ar,myid,par[2],par[0],par[1],par[3],par[4]){}
 
 Species::Species(Arena *ar,int myid, double death, double growth, double rep=0, double radius=0, double maxEf=0)
-		:id(myid),G(growth),D(death),R(rep),Rad(radius),maxStressEffect(maxEf){
-	int i;
-	nextStage = NULL;
-	seedStage = NULL;
-	dispersalRadius = 0;
+    :id(myid),G(growth),D(death),R(rep),Rad(radius),maxStressEffect(maxEf)
+{
+    int i;
+    nextStage = NULL;
+    seedStage = NULL;
+    dispersalRadius = 0;
 
-	arena = ar;
-	spnum = ar->getSpNum();
+    arena = ar;
+    spnum = ar->getSpNum();
 
-	interactions = (double*)malloc(spnum*(sizeof(double)));
-	for(i=1;i<=spnum;i++){
-		interactions[i]=0;
-	}
-	interactionVariation=0;
+    interactions = (double*)malloc(spnum*(sizeof(double)));
+    for(i=1;i<=spnum;i++){
+        interactions[i]=0;
+    }
+    interactionVariation=0;
+}
 
 Species::~Species(){
-	/* clear population */
-	std::list<Individual*>::iterator i;
+    /* clear population */
+    std::list<Individual*>::iterator i;
 
-	for(i=population.begin();i!=population.end();i++){
-		delete(*i);
-	}
+    for(i=population.begin();i!=population.end();i++){
+        delete(*i);
+    }
 
-	free(interactions);
+    free(interactions);
 }
 
 void Species::setFacilitation(double f){setInteraction(spnum,f);}
 void Species::setAutoInteraction(double effect){setInteraction(id,effect);}
 
 void Species::setInteraction(int s, double effect){
-	if(effect > D){
-		Rcpp::warning("Interaction parameter set to be bigger than deathrate.");
-	}
-	interactions[s] = effect;
+    if(effect > D){
+        Rcpp::warning("Interaction parameter set to be bigger than deathrate.");
+    }
+    interactions[s] = effect;
 }
 
 void Species::addIndividual(double x, double y){
-	if(G > 0 && nextStage==NULL) {
-		Rcpp::warning("Next stage set to NULL but G > 0. Check input data.");
-		throw id;
-	}
-	if(R > 0 && seedStage==NULL) {
-		Rcpp::warning("Seed stage set to NULL but R > 0. Check input data.");
-		throw id;
-	}
-	/*Individual *i =*/ new Individual(arena,this,x,y);
+    if(G > 0 && nextStage==NULL) {
+        Rcpp::warning("Next stage set to NULL but G > 0. Check input data.");
+        throw id;
+    }
+    if(R > 0 && seedStage==NULL) {
+        Rcpp::warning("Seed stage set to NULL but R > 0. Check input data.");
+        throw id;
+    }
+    /*Individual *i =*/ new Individual(arena,this,x,y);
 }
 
 void Species::addIndividual(Position p){
-	addIndividual(p.x,p.y);
+    addIndividual(p.x,p.y);
 }
 
 void Species::disperseIndividual(double x, double y){
-	Position p(x,y);
-	disperseIndividual(p);
+    Position p(x,y);
+    disperseIndividual(p);
 }
 
 void Species::disperseIndividual(Position p){
-	addIndividual(p + dispersalKernel());
+    addIndividual(p + dispersalKernel());
 }
 
 Position Species::dispersalKernel(){
-	Position p;
-	switch(kernelType){
-		case 0: /* Fully random on the arena */
-			return Position(Random(arena->getWidth()),Random(arena->getHeight()));
-		case 1: /* EXPONENTIAL */
-		default:
-			if(dispersalRadius <= 0) return Position(0,0);
-			p = RandomDirection();
-			return Exponential(1.0/dispersalRadius)*p;
-	}
+    Position p;
+    switch(kernelType){
+        case 0: /* Fully random on the arena */
+            return Position(Random(arena->getWidth()),Random(arena->getHeight()));
+        case 1: /* EXPONENTIAL */
+        default:
+            if(dispersalRadius <= 0) return Position(0,0);
+            p = RandomDirection();
+            return Exponential(1.0/dispersalRadius)*p;
+    }
 }
 
 double Species::getTotalRate(){
-	totalRate = 0;
-	std::list<Individual*>::iterator i;
+    totalRate = 0;
+    std::list<Individual*>::iterator i;
 
-	for(i=population.begin();i!=population.end();i++){
-		totalRate += (*i)->getTotalRate();
-	}
+    for(i=population.begin();i!=population.end();i++){
+        totalRate += (*i)->getTotalRate();
+    }
 
-	return totalRate;
+    return totalRate;
 }
 
 bool Species::isPresent(Position p, double radius) {
-	std::list<Individual*>::iterator i;
+    std::list<Individual*>::iterator i;
 
-	for(i=population.begin();i!=population.end();i++){
-		if((*i)->isPresent(p,radius*radius)) return true;
-	}
+    for(i=population.begin();i!=population.end();i++){
+        if((*i)->isPresent(p,radius*radius)) return true;
+    }
 
-	return false;
+    return false;
 }
 
 std::list<Individual*> Species::getPresent(Position p, double radius){
-	std::list<Individual*>::iterator i;
-	std::list<Individual*> list;
+    std::list<Individual*>::iterator i;
+    std::list<Individual*> list;
 
-	for(i=population.begin();i!=population.end();i++){
-		if((*i)->isPresent(p,radius*radius)) list.push_back(*i);
-	}
+    for(i=population.begin();i!=population.end();i++){
+        if((*i)->isPresent(p,radius*radius)) list.push_back(*i);
+    }
 
-	return list;
+    return list;
 }
 
 void Species::act(){
-	std::list<Individual*>::iterator i;
-	double r = Random(totalRate);
+    std::list<Individual*>::iterator i;
+    double r = Random(totalRate);
 
-	for(i=population.begin();i!=population.end();i++){
-		r -= (*i)->getTotalRate();
-		if(r < 0) {
-			(*i)->act();
-			return;
-		}
-	}
-	/* if the below code is executed, it's becase no individual was selected */
-	Rcpp::warning ("No individual selected on Species::act");
+    for(i=population.begin();i!=population.end();i++){
+        r -= (*i)->getTotalRate();
+        if(r < 0) {
+            (*i)->act();
+            return;
+        }
+    }
+    /* if the below code is executed, it's becase no individual was selected */
+    Rcpp::warning ("No individual selected on Species::act");
 }
 
 void Species::setNextStage(Species *st) {nextStage = st;}
 void Species::setSeedStage(Species *st, double dispersal, int kernel) {
-	seedStage = st;
-	dispersalRadius = dispersal;
-	kernelType=kernel;
+    seedStage = st;
+    dispersalRadius = dispersal;
+    kernelType=kernel;
 }
 
 void Species::remove(std::list<Individual*>::iterator i){
-	population.erase(i);
+    population.erase(i);
 }
 
 std::list<Individual*>::iterator Species::add(Individual *i){
-	population.push_front(i);
-	return population.begin();
+    population.push_front(i);
+    return population.begin();
 }
 
 Species* Species::getSeedStage() {return seedStage;}
@@ -150,16 +152,16 @@ double Species::getRad(){return Rad;}
 double Species::getInteraction(int species_id, Position p){return interactions[species_id]+arena->getStressValue(p)*interactionVariation;}
 int Species::getId(){return id;}
 double Species::getD(Position p){
-	if(maxStressEffect == 0){
-		return D;
-	}
-	else {
-		return arena->getStressValue(p)*maxStressEffect;
-	}
+    if(maxStressEffect == 0){
+        return D;
+    }
+    else {
+        return arena->getStressValue(p)*maxStressEffect;
+    }
 }
 
 int Species::getAbundance(){
-	return population.size();
+    return population.size();
 }
 
 void Species::setInteractionVariation(double maxeffect){interactionVariation=maxeffect;}
