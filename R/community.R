51baa3e9ca44db918760e59d5500babcc5a9d59e
Oz <marinacs@gmail.com>
2017-05-01 22:03:46 -0300

    primeira verão funcional, spatial plot ainda n funciona)

--- R/community.R
+++ R/community.R
@@ -4,18 +4,14 @@
 #' effects decrease death rates while negative ones increase death rates.
 #' 
 #' @param maxtime 	How long the simulation must run
-#' @param n 		Array of number of stages for each population
-#' @param D		List of lenght N containing arrays of death rates for each stage of each population
-#' @param G		List of lenght N containing arrays of growth rates for each stage of each population
-#' @param R		List of lenght N containing arrays of seed production rates for each stage of each population
-#' @param init		List of lenght N containing arrays of initial numbers for each stage of the
-#' structured populations
-#' @param dispersal	List of lenght N containing average distances for seed dispersal for each
-#' population
-#' @param maxstresseffects		Optional (use for a stress gradient). Array of values for
-#' how much the environmental stress can increse death rate at maximum stress. Length n+1. Stress
-#' gradient is linear, being miminum a x=0 (left) and maximum at x=width (right).
-#' @param radius		Optional (use if there are any interactions). Array of interaction radiuses. Length n+1.
+#' @param numstages Array of number of stages for each population
+#' @param parameters 	Matrix with one row for each stage. Columns:
+#' D,G,R,radius(optional),maxstressefect (optional)
+#' @param init		Either an array of initial numbers for each stage of each population, or a
+#' data.frame with the history of a simulation
+#' @param dispersal	Average distance for seed dispersal
+#' @param maxstresseffects		Optional (use for a stress gradient). Array of values for the slope
+#' of increase of environmental effect on each stage
 #' @param interactions	Optional. An array of effects of life stages over each other, where element
 #' [i+n*j] means the effect of stage i over stage j. Positive values equal facilitation, negative ones, competition.
 #' @param height	Arena height
@@ -28,18 +24,14 @@
 #' @param maxpop	If the simulation reaches this many individuals total, it will stop. Default
 #' is 30000.
 #' @examples
-#' d <- list(c(5,2,.1),c(2,1))
-#' g <- list(c(5,2),c(1))
-#' rep <- list(c(0,1,10),c(0,8))
 #' init <- list(c(100,0,0),c(100,0))
 #' malth <- community(2,c(3,2),d,g,rep,dispersal=2,init=init)
 #' times <- seq(0,2,by=0.1)
 #' ab <- abundance_matrix(malth,times)
 #' stackplot(ab[,1:3]) # species 1
 #' stackplot(ab[,4:5]) # species 2
-community <- function(maxtime, n, D, G, R, dispersal, init, # the main parameters
-                         maxstresseffects = rep(0,sum(n)), radius=rep(2,sum(n)), # stress gradient effects
-                         interactions=rep(0,sum(n)*sum(n)), # interactions
+community <- function(maxtime, numstages, parameters, dispersal, init, # the main parameters
+                         interactions=F, # interactions
                          height=100, width=100, boundary=c("reflexive","absortive","periodic"), # arena properties
                          dispKernel=c("exponential","random"), # type of dispersal
                          maxpop=30000){
@@ -50,24 +42,24 @@ community <- function(maxtime, n, D, G, R, dispersal, init, # the main parameter
 	boundary <- match.arg(boundary)
 	bound <- switch(boundary,reflexive=1,absortive=0,periodic=2)
 
-    ntot <- sum(n)
-	if(length(radius)==1) radius <- rep(radius,ntot) # if only one radius specified, everybody has that radius
+    ntot <- sum(numstages)
+    npop <- length(numstages)
 
-    Ds <- unlist(D)
-    if(length(G)>0){
-        Gs <- unlist(sapply(G,function(i){c(i,0)}))
+	if(interactions==F){
+        N = matrix(rep(0,ntot*ntot),ntot)
     }
-    else{
-        G <- 0
+    else {
+        N = matrix(interactions,ntot)
     }
-    Rs <- unlist(R)
-	M <- t(matrix(c(
-            Ds, Gs, Rs,
-			unlist(radius), #Rads
-			unlist(maxstresseffects) #effects
-		), nrow = ntot))
 
-	N <- matrix(interactions,nrow=ntot)
+    M <- parameters
+    if(ncol(M)==4){ # assume maxstresseffect is missing
+        M <- cbind(M,rep(0,ntot))
+    }
+    else if(ncol(M)==3){ # assume radius and maxstress effect are missing
+        M <- cbind(M,rep(1,ntot))
+        M <- cbind(M,rep(0,ntot))
+    }
 
     # generate init parameter
     restore=F
@@ -85,7 +77,7 @@ community <- function(maxtime, n, D, G, R, dispersal, init, # the main parameter
     }
 
 	# run simulation
-	r <- simulation(maxtime,num_pops=length(n),num_stages=n,parameters=c(M),dispersal=dispersal,interactions=N,
+	r <- simulation(maxtime,num_pops=npop,num_stages=numstages,parameters=c(t(M)),dispersal=dispersal,interactions=N,
                     init=initial,history=hist,restore=restore,h=height,w=width,bcond=bound,dkernel=disp,maxpop=maxpop)
 
 
@@ -99,8 +91,10 @@ community <- function(maxtime, n, D, G, R, dispersal, init, # the main parameter
 	N <- matrix(N,nrow=ntot)
 	rownames(N) <- 1:ntot
 	colnames(N) <- 1:ntot
+    rownames(M) <- 1:ntot
+    colnames(M) <- c("D","G","R","radius","maxstresseffect")
 
-	list(data = r,n=n+1, maxtime=maxtime,
-	     stages=n,D=D,G=G,R=R,radius=radius,dispersal=dispersal,interactions=N,interactions.vec=interactions,rates.matrix=M,
+	list(data = r,num.pop = npop, num.total = ntot, num.stages = numstages, maxtime=maxtime,
+	     dispersal=dispersal,interactions=N,rates.matrix=M,radius=M[,4],
 	     init=init,height=height,width=width,boundary=boundary,dispKernel=dispKernel)
 }
