9ad2c16fb10e56cc7943af435c219fadaa870b89
Oz <marinacs@gmail.com>
2017-09-30 15:14:45 -0300

    added test. fix #44

--- examples/neutral_model.R
+++ examples/neutral_model.R
@@ -1,39 +1,116 @@
 
 maxt<-60
-dispersal<-30
 
-### Four species two stages competition 
-nsp <- 100
-nst <- 2
-init <- c(20,20)
-nstages <- rep(nst,nsp)
-totstages <- nst*nsp
-init <- rep(list(init),nsp)
-par <- c(2,1,0,1, 1,0,5,2)
-param <- matrix(rep(par,nsp)
-                , byrow=T, nrow=sum(nstages)) 
-sapsap <- -1
-adusap <- 0
-intersap <- rep(c(sapsap,adusap),nsp)
-aduadu <- 0
-sapadu <- 0
-interadu <- rep(c(sapadu,aduadu),nsp)
-interact <- matrix(c(rep(c(intersap,interadu),nsp)),ncol=totstages)
+neutralsimD <- function(nads){
 
-neutral <- community(maxt,nstages,param,dispersal,init,interactions=interact,h=50,w=50,maxpop=300000)
+    area<-nads/.12
+    nsp <- floor(nads/10) # floor because less is better than more
 
-neutral$radius <- rep(c(.2,.4),nsp)
+    dispersal<-30
+    h<-w<-sqrt(area)
+    nst <- 2
+    init <- c(10,10)
+    nstages <- rep(nst,nsp)
+    totstages <- nst*nsp
+    init <- rep(list(init),nsp)
+    par <- c(4,1,0,1, 1,0,5.5,2)
+    param <- matrix(rep(par,nsp)
+                    , byrow=T, nrow=sum(nstages))
+    sapsap <- -2
+    adusap <- 0
+    intersap <- rep(c(sapsap,adusap),nsp)
+    aduadu <- 0
+    sapadu <- 0
+    interadu <- rep(c(sapadu,aduadu),nsp)
+    interact <- matrix(c(rep(c(intersap,interadu),nsp)),ncol=totstages)
+
+    neutral <- community(maxt,nstages,param,dispersal,init,interactionsD=interact,h=h,w=w)
+
+    save(neutral,file="test.RData")
+
+    neutral
+}
+
+n <- neutralsimD(400)
+nsp <- n$num.pop
+adults <- 1:nsp*2
+juvs <- adults-1
+b<-abundance.matrix(n,seq(10,n$maxtime,length.out=20))
+bads<-b[,adults]
+summary(c(b[ ,juvs]/bads)) # average juv:adult ratio
+summary(rowSums(bads/(n$h*n$w))) # number of adults per square unit
+summary(rowSums(bads)) # should average very close to the expected number
+summary(rowSums(b)) # estimate of number of individuals
+stackplot(bads)
+
+neutralsimG <- function(nads){
+
+    area<-nads/.12
+    nsp <- floor(nads/10) # floor because less is better than more
+
+    dispersal<-30
+    h<-w<-sqrt(area)
+    nst <- 2
+    init <- c(45,10)
+    nstages <- rep(nst,nsp)
+    totstages <- nst*nsp
+    init <- rep(list(init),nsp)
+    par <- c(2,4,0,1, 1,0,10,2)
+    param <- matrix(rep(par,nsp)
+                    , byrow=T, nrow=sum(nstages))
+
+    # growthrate interaction
+    sapsap <- 0
+    adusap <- -4 # saplings cannot become adults under an adult 
+    intersap <- rep(c(sapsap,adusap),nsp)
+    aduadu <- 0
+    sapadu <- 0
+    interadu <- rep(c(sapadu,aduadu),nsp)
+    interactG <- matrix(c(rep(c(intersap,interadu),nsp)),ncol=totstages)
+
+
+    neutral <- community(maxt,nstages,param,dispersal,init,interactionsG=interactG,h=h,w=w)
+
+    save(neutral,file="test.RData")
+
+    neutral
+}
+
+n <- neutralsimG(400)
+nsp <- n$num.pop
+adults <- 1:nsp*2
+juvs <- adults-1
+b<-abundance.matrix(n,seq(10,n$maxtime,length.out=20))
+bads<-b[,adults]
+summary(c(b[ ,juvs]/bads)) # average juv:adult ratio
+summary(rowSums(bads/(n$h*n$w))) # number of adults per square unit
+summary(rowSums(bads)) # should average very close to the expected number
+summary(rowSums(b)) # estimate of number of individuals
+stackplot(bads)
+
+
+adultpops <- function(model,t) {
+    pop<-c(abundance.matrix(model,t)[,(1:nsp)*nst])
+    pop<-pop[pop>0]
+    pop
+}
+
+neutral<-neutralsim(200)
+
+nsp<-neutral$num.pop
+nst<-neutral$num.stages[1]
+#neutral$radius <- rep(c(.2,.4),nsp)
 a<-abundance.matrix(neutral)
 stackplot(a)
-last<- c(abundance.matrix(neutral,neutral$maxtime)[,(1:nsp)*nst])
+last<- adultpops(neutral,neutral$maxtime)
 plot(sort(last))
 plotsnapshot(neutral,neutral$maxtime)
 
-adusapfac <- +.1
-intersap <- c(rep(c(sapsap,adusap),nsp/2),rep(c(sapsap,adusapfac),nsp/2)) # half of the species are facilitators, all saplings receive facilitation
-interact <- matrix(c(rep(c(intersap,interadu),nsp)),ncol=totstages)
+library(sads)
+pops<-adultpops(neutral,60)
+plot(octav(adultpops(neutral,50)))
+
+adultpops(neutral,30)->pops
 
 
-facil <- community(maxt,nstages,param,dispersal,init,interactions=interact,h=50,w=50,maxpop=300000)
-a<-abundance.matrix(facil)
-stackplot(a)
+
--- examples/test_community.R
+++ examples/test_community.R
@@ -1,5 +1,5 @@
 maxt <- 30
-dispersal<-2
+dispersal<-20
 
 results<-list()
 
@@ -18,23 +18,23 @@ results$malth2 <- community(maxt,nstages,param,dispersal,init)
 ### Logistic one species
 nstages <- 3
 init <- c(0,0,100)
-param <- matrix(c(5,1,0,0, 1,1,0,0, .5,0,10,1),nrow=3,byrow=T)
+param <- matrix(c(5,1,0,0, 1,1,0,0, .5,0,10,2),nrow=3,byrow=T)
 interact <- matrix(c(0,0,0,0,0,0,0,0,-.1),ncol=3)
-results$compet1 <- community(maxt,nstages,param,dispersal,init,interactions=interact)
+results$compet1 <- community(maxt,nstages,param,dispersal,init,interactionsD=interact)
 
 ### Two species competition (different is better)
 nstages <- c(3,2)
 init <- list(c(100,0,10),c(100,30))
 param <- matrix(c(2,1,0,0, 1,1,0,.1, .5,0,6,1, 1,1,0,.2, .5,0,2,2), byrow=T, nrow=5) 
 interact <- matrix(c(0,0,0,0,0, 0,0,0,0,0, 0,0,-.05,0,-.2, 0,0,0,0,0, 0,0,-.05,0,-.1),ncol=5)
-results$compet2diff <- community(maxt,nstages,param,dispersal,init,interactions=interact)
+results$compet2diff <- community(maxt,nstages,param,dispersal,init,interactionsD=interact)
 
 ### Two species competition (same is better)
 nstages <- c(3,2)
 init <- list(c(100,0,10),c(100,30))
 param <- matrix(c(2,1,0,0, 1,1,0,.1, .5,0,6,1, 1,1,0,.2, .5,0,2,2), byrow=T, nrow=5) 
 interact <- matrix(c(0,0,0,0,0, 0,0,0,0,0, 0,0,-.1,0,-.2, 0,0,0,0,0, 0,0,-.2,0,-.1),ncol=5)
-results$compet2same <- community(maxt,nstages,param,dispersal,init,interactions=interact)
+results$compet2same <- community(maxt,nstages,param,dispersal,init,interactionsD=interact)
 
 ### Two species competition facilitation
 nstages <- c(3,2)
@@ -42,7 +42,7 @@ init <- list(c(100,0,10),c(100,30))
 param <- matrix(c(2,1,0,0, 1,1,0,.1, .5,0,6,1, 1,1,0,.2, .5,0,2,2), byrow=T, nrow=5) 
 interact <- matrix(c(0,0,0,0,0, 0,-.1,+.1,-.1,0, 0,0,-.1,0,-.2, 
                                 0,-.2,+.2,-.1,0, 0,0,-.2,0,-.1),ncol=5)
-results$competfac2 <- community(maxt,nstages,param,dispersal,init,interactions=interact)
+results$competfac2 <- community(maxt,nstages,param,dispersal,init,interactionsD=interact)
 
 ### Three species competition facilitation
 nstages <- c(3,2,1)
@@ -53,6 +53,6 @@ param <- matrix(c(2,1,0,0, 1,1,0,.1, .5,0,6,1,
 interact <- matrix(c(0,0,0,0,0,-.1, 0,-.1,+.1,-.1,0,+.1, 0,0,-.1,0,-.2,-.1, 
                                     0,-.2,+.2,-.1,0,+.1, 0,0,-.2,0,-.1,-.1,
                                                          0,0,-.01,0,-.01,-.02),ncol=6)
-results$competfac3 <- community(maxt,nstages,param,dispersal,init,interactions=interact)
+results$competfac3 <- community(maxt,nstages,param,dispersal,init,interactionsD=interact)
 
 abunds <- lapply(results,abundance.matrix)
